538b09bd4ec032c9ad39f3d7d7a71444248e9742
Brianrafs <brianxdgmr@gmail.com>
2024-07-10 11:33:31 -0300

    feat: Create inference model

---
+++ api/src/models/inference.py
@@ -0,0 +1,12 @@
+class InferenceRequest(BaseModel):
+    label_avg_price_per_room: int
+    no_of_adults: int
+    no_of_children: int
+    required_car_parking_space: int
+    arrival_year: int
+    arrival_month: int
+    no_of_special_requests: int
+    type_of_meal_plan: str
+    room_type_reserved: str
+    market_segment_type: str
+    booking_status: str
