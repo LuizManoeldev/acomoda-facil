480f83f4db2d1dfbfc145071cfaec9e21ee1aa44
LuizManoeldev <dantasluizmanoel@gmail.com>
2024-07-13 16:01:37 -0300

    chore: rafactoring directory structure

--- scripts/data_clean.py
+++
@@ -1,9 +0,0 @@
-from get_rds_data import base_reservations, engine
-
-# TEST
-try:
-    # PREPARAR DADOS...
-    base_reservations.to_sql('reservations_test', con=engine, if_exists='replace', index=False)
-    print("DataFrame written to SQL table successfully.")
-except Exception as e:
-    print(f"Erro ao escrever no banco de dados: {e}")
