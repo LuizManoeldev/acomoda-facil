1e86445d8e867320c34e676801ae9384057bf3c7
LuizManoeldev <dantasluizmanoel@gmail.com>
2024-07-13 17:30:13 -0300

    chore: configuration path exemple

---
+++ environment/path_exemple.py
@@ -0,0 +1,13 @@
+import sys
+
+# Modelo
+sys.path.append('/caminho/caminho/pasta_do_projeto/diretorios...')
+
+# Caminhos a serem criados seguindo minha estrutura
+sys.path.append('/home/luizdantas/sprints-4-5-pb-aws-maio')
+sys.path.append('/home/luizdantas/sprints-4-5-pb-aws-maio/data')
+sys.path.append('/home/luizdantas/sprints-4-5-pb-aws-maio/data/processed')
+sys.path.append('/home/luizdantas/sprints-4-5-pb-aws-maio/data/external')
+sys.path.append('/home/luizdantas/sprints-4-5-pb-aws-maio/notebooks')
+sys.path.append('/home/luizdantas/sprints-4-5-pb-aws-maio/notebooks/exploratory')
+sys.path.append('/home/luizdantas/sprints-4-5-pb-aws-maio/notebooks/modeling')
