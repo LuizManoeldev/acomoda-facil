555d690e1eeabfee2f3c1c1fd2a4144414bf290f
LuizManoeldev <dantasluizmanoel@gmail.com>
2024-07-13 23:37:57 -0300

    chore: update path exemple

--- environment/path_exemple.py
+++ environment/path_exemple.py
@@ -1,13 +1,10 @@
 import sys
 
-# Modelo
-sys.path.append('/caminho/caminho/pasta_do_projeto/diretorios...')
+# Diretorios a serem adicionados ao path do seu python env
 
-# Caminhos a serem criados seguindo minha estrutura
-sys.path.append('/home/luizdantas/sprints-4-5-pb-aws-maio')
-sys.path.append('/home/luizdantas/sprints-4-5-pb-aws-maio/data')
-sys.path.append('/home/luizdantas/sprints-4-5-pb-aws-maio/data/processed')
-sys.path.append('/home/luizdantas/sprints-4-5-pb-aws-maio/data/external')
-sys.path.append('/home/luizdantas/sprints-4-5-pb-aws-maio/notebooks')
-sys.path.append('/home/luizdantas/sprints-4-5-pb-aws-maio/notebooks/exploratory')
-sys.path.append('/home/luizdantas/sprints-4-5-pb-aws-maio/notebooks/modeling')
+sys.path.append('...seu_projeto/data')
+sys.path.append('...seu_projeto/data/processed')
+sys.path.append('...seu_projeto/data/external')
+sys.path.append('...seu_projeto/notebooks')
+sys.path.append('...seu_projeto/notebooks/exploratory')
+sys.path.append('...seu_projeto/notebooks/modeling')
